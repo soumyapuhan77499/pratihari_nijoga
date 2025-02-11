ce6e7a618279cee8d67cbbe9d072fd8b035324a4
soumyaranjanpuhan <soumyaranjan.puhan@33crores.com>
2025-02-11 14:42:55 +0530

    Add debug statement to saveProfile method to inspect authenticated user

--- app/Http/Controllers/Api/PratihariProfileApiController.php
+++ app/Http/Controllers/Api/PratihariProfileApiController.php
@@ -17,6 +17,7 @@ class PratihariProfileApiController extends Controller
     try {
           // Authenticate user
           $user = Auth::guard('sanctum')->user();
+          dd($user);
           if (!$user) {
               return response()->json([
                   'status' => 401,
